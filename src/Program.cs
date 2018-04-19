0a819e28e2860c9349235bbcdd9a1a8d30eb2b17
PhantomCloak <34552014+PhantomCloak@users.noreply.github.com>
2018-04-19 22:34:19 +0300

    Add files via upload

---
+++ src/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UACBypass;
+namespace UACBypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool oldMethod = true;
+            Console.WriteLine("Path To Elevate : ");
+            string path_to_elevate = Console.ReadLine();
+
+            if(oldMethod)
+            {
+                //path_to_elevate immediately process
+                BypassUAC.AttempOldMethod(path_to_elevate);
+            }else
+            {
+                //Next boot-up will elevate process
+                BypassUAC.AttemptNewMethod(path_to_elevate);
+            }
+        }
+    }
+}
